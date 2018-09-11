dfaf31dd2a99f87c890ab8b538091d72085be814
Daniel Errante <danoph@gmail.com>
2018-09-11 13:57:30 -0500

    working on frame scorer

--- src/bowling/frame.scorer.ts
+++ src/bowling/frame.scorer.ts
@@ -4,8 +4,23 @@ export class FrameScorer {
   scoreFrames(frames: IFrame[]): number {
     let score = 0;
 
-    for (let frame of frames) {
-      score += frame.score;
+    let frameScore = 0;
+    let previousFrame: any;
+
+    for (let frameNumber in frames) {
+      const frame = frames[frameNumber];
+
+      if (parseInt(frameNumber) > 0) {
+        previousFrame = frames[parseInt(frameNumber) - 1];
+      }
+
+      if (previousFrame && previousFrame.score === 20) {
+        frameScore += 20;
+      } else {
+        frameScore = 0;
+      }
+
+      score += frame.score + frameScore;
     }
 
     return score;
