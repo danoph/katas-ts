dd199b722bb962b31d0772537df4bd90d3b0f488
Daniel Errante <danoph@gmail.com>
2018-09-11 11:58:58 -0500

    starting point

--- src/bowling/game.ts
+++ src/bowling/game.ts
@@ -1,3 +1,13 @@
+export const BOWLING_GAME_TOO_SHORT = 'BOWLING_GAME_TOO_SHORT';
+export const BOWLING_GAME_TOO_LONG = 'BOWLING_GAME_TOO_LONG';
+export const BOWLING_SPARE_TOO_EARLY = 'BOWLING_SPARE_TOO_EARLY';
+export const BOWLING_STRIKE_TOO_LATE = 'BOWLING_STRIKE_TOO_LATE';
+export const BOWLING_TOO_MANY_PINS = 'BOWLING_TOO_MANY_PINS';
+
 export class Game {
-  constructor(private frames: string) {}
+  constructor(framesString: string) {}
+
+  score() {
+    return 0;
+  }
 }
