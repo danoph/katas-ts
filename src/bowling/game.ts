5e41d8c7309aa365cbfc7f712c1763ae77fe2ef2
Daniel Errante <danoph@gmail.com>
2018-09-11 11:03:10 -0500

    working on tests

--- src/bowling/game.ts
+++ src/bowling/game.ts
@@ -1,3 +1,27 @@
+export class BowlingError extends Error {
+  constructor(message) {
+    super(message);
+    Object.setPrototypeOf(this, BowlingError.prototype);
+    this.name = this.constructor.name;
+  }
+
+  dump() {
+    return { message: this.message, stack: this.stack }
+  }
+}
+
+export class BowlingGameTooShort extends BowlingError { }
+export class BowlingGameTooLong extends BowlingError {}
+export class BowlingSpareTooEarly extends BowlingError {}
+export class BowlingStrikeTooLate extends BowlingError {}
+export class BowlingTooManyPins extends BowlingError {}
+
 export class Game {
-  constructor(private frames: string) {}
+  constructor(private frames: string) {
+    throw new Error("Game too short");
+  }
+
+  score() {
+    return 0;
+  }
 }
